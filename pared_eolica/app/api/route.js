008c9ce8a50a4936ad4a9512ae2f82c1f1337ca0
AlfredoB14 <alfredobarranco14@gmail.com>
2024-08-30 14:11:42 -0600

    refactor: Update wallDataAPI route to use MongoDB for data storage

--- pared_eolica/app/api/route.js
+++ pared_eolica/app/api/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from "@/libs/prisma";
 
 export async function POST(request) {
     try {
