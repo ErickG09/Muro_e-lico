de179a4feed70a4abcc9a5677e57e9bd22e38a52
AlfredoB14 <alfredobarranco14@gmail.com>
2024-08-30 14:08:11 -0600

    l

--- pared_eolica/app/api/route.js
+++ pared_eolica/app/api/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prisma";
 
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export async function POST(request) {
     try {
       const data = await request.json();
