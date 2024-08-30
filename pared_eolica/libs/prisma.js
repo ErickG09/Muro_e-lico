9cceb71a8235525c6aa36fbb447831d9f478c554
AlfredoB14 <alfredobarranco14@gmail.com>
2024-08-30 14:13:48 -0600

    chore: Update Prisma initialization logic in prisma.js

--- pared_eolica/libs/prisma.js
+++ pared_eolica/libs/prisma.js
@@ -2,13 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-} else {
+if (process.env.NODE_ENV === "development") {
     if (!global.prisma) {
         global.prisma = new PrismaClient();
     }
+
     prisma = global.prisma;
+}
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
 }
 
 export default prisma;
