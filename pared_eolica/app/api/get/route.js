7547850f7d3ad25945ebfe5a4793de9b92afb74c
AlfredoB14 <alfredobarranco14@gmail.com>
2024-08-31 09:01:52 -0600

    feat: Remove deprecated route for retrieving propeller data
    The deleted file `route.js` contained an outdated implementation for retrieving propeller data. This commit removes the unnecessary code and improves the codebase by eliminating unused files.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (feat for feature, refactor for code refactoring, chore for maintenance tasks) and provides a clear and concise description of the changes made.

--- pared_eolica/app/api/get/route.js
+++
@@ -1,9 +0,0 @@
-import { sql, db } from "@vercel/postgres";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = await sql 
-  `select * from PropellerData`;
-
-  return NextResponse.json({ data });
-}
