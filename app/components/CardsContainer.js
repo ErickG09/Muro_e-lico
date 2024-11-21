a6d2a75d9c21684f4d485b50c418b59658778664
Erick <witherick11@gmail.com>
2024-11-21 14:24:33 -0600

    cambios texto posicion

--- app/components/CardsContainer.js
+++ app/components/CardsContainer.js
@@ -27,7 +27,7 @@ const fadeSlideOutToLeft = keyframes`
 const CardsContainer = ({ latestData, isExiting }) => {
   return (
     <Flex 
-      direction="row"
+      direction="column"
       align="center"
       marginBottom="20px" 
       backgroundColor="#E9ECEF" 
@@ -44,13 +44,12 @@ const CardsContainer = ({ latestData, isExiting }) => {
       <Text fontSize="xl" fontWeight="bold" mb="10px">
         Group 1
       </Text>
-
       <Flex justify="space-between" align="center" width="100%" gap="20px">
-        <PropellerCard title="Propeller 1" percentage={Math.floor((latestData?.propeller1 * 100) / 5) || 0} voltage={`${(latestData?.propeller1 ** 2/216 * 1000).toFixed(2) || 0} mW`} />
-        <PropellerCard title="Propeller 2" percentage={Math.floor((latestData?.propeller2 * 100) / 5)|| 0} voltage={`${(latestData?.propeller2 ** 2/216 * 1000).toFixed(2) || 0} mW`} />
-        <PropellerCard title="Propeller 3" percentage={Math.floor((latestData?.propeller3 * 100) / 5)|| 0} voltage={`${(latestData?.propeller3 ** 2/216 * 1000).toFixed(2) || 0} mW`} />
-        <PropellerCard title="Propeller 4" percentage={Math.floor((latestData?.propeller4 * 100) / 5)|| 0} voltage={`${(latestData?.propeller4 ** 2/216 * 1000).toFixed(2) || 0} mW`} />
-        <PropellerCard title="Propeller 5" percentage={Math.floor((latestData?.propeller5 * 100) / 5)|| 0} voltage={`${(latestData?.propeller5 ** 2/216 * 1000).toFixed(2) || 0} mW`} />
+        <PropellerCard title="Propeller 1" percentage={Math.floor((latestData?.propeller1 * 100) / 5) || 0} voltage={`${latestData?.propeller1 * 0.01.toFixed(2) || 0} mW`} />
+        <PropellerCard title="Propeller 2" percentage={Math.floor((latestData?.propeller2 * 100) / 5) || 0} voltage={`${latestData?.propeller2 * 0.01.toFixed(2) || 0} mW`} />
+        <PropellerCard title="Propeller 3" percentage={Math.floor((latestData?.propeller3 * 100) / 5) || 0} voltage={`${latestData?.propeller3 * 0.01.toFixed(2) || 0} mW`} />
+        <PropellerCard title="Propeller 4" percentage={Math.floor((latestData?.propeller4 * 100) / 5) || 0} voltage={`${latestData?.propeller4 * 0.01.toFixed(2) || 0} mW`} />
+        <PropellerCard title="Propeller 5" percentage={Math.floor((latestData?.propeller5 * 100) / 5) || 0} voltage={`${latestData?.propeller5 * 0.01.toFixed(2) || 0} mW`} />
       </Flex>
     </Flex>
 
